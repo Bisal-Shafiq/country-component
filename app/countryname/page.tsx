6e6da77661ecc4e30242e6938dc33e7234bd676d
Bisal-Shafiq <93635077+Bisal-Shafiq@users.noreply.github.com>
2024-10-18 15:58:17 +0500

    Update page.tsx

--- app/countryname/page.tsx
+++ app/countryname/page.tsx
@@ -2,15 +2,114 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <div>
-            <ul>
-                <li><Link href="/countryname/pakistan">Pakistan</Link></li>
-                <li><Link href="/countryname/korea">Korea</Link></li>
-                <li><Link href="/countryname/japan">Japan</Link></li>
-                <li><Link href="/countryname/india">India</Link></li>
-                <li><Link href="/countryname/iran">Iran</Link></li>
-                <li><Link href="/countryname/iraq">Iraq</Link></li>
-            </ul>
+        <div style={{ textAlign: "center", padding: "20px" }}>
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/pakistan" style={{ textDecoration: "none", color: "white" }}>
+                    Pakistan
+                </Link>
+            </button>
+
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/korea" style={{ textDecoration: "none", color: "white" }}>
+                    Korea
+                </Link>
+            </button>
+
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/japan" style={{ textDecoration: "none", color: "white" }}>
+                    Japan
+                </Link>
+            </button>
+
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/india" style={{ textDecoration: "none", color: "white" }}>
+                    India
+                </Link>
+            </button>
+
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/iran" style={{ textDecoration: "none", color: "white" }}>
+                    Iran
+                </Link>
+            </button>
+
+            <button
+                style={{
+                    margin: "10px",
+                    borderRadius: "10px",
+                    border: "2px solid black",
+                    padding: "10px 20px",
+                    backgroundColor: "#f79eb3",
+                    cursor: "pointer",
+                    fontSize: "16px",
+                    textAlign: "center",
+                    width: "150px"
+                }}
+            >
+                <Link href="/countryname/iraq" style={{ textDecoration: "none", color: "white" }}>
+                    Iraq
+                </Link>
+            </button>
         </div>
     );
 }
