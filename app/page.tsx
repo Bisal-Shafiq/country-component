3b42804d70ca83c49e9293958416b9e5b89071dd
Bisal-Shafiq <93635077+Bisal-Shafiq@users.noreply.github.com>
2024-10-18 15:58:46 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-
-
 export default function Home() {
   return (
-    <div>
-    <h1>Home Page</h1>
-    
+    <div style={{ textAlign: "center", padding: "20px", backgroundColor: "#f8f9fa" }}>
+      <h1 style={{ color: "blue", fontSize: "32px", margin: "20px 0" }}>This is Home Page</h1>
     </div>
-  )
+  );
 }
